a0dc2a9122fc2d4d3e22a5d77535685b932530f2
Livku <wellingtonhilbert@outlook.com>
2025-07-20 21:17:58 +0100

    Delete XRImGUILib/app/build directory

--- XRImGUILib/app/build/intermediates/cxx/Debug/n6l545p2/logs/arm64-v8a/build_command_xrimguilib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\John\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\John\\AndroidStudioProjects\\XRImGUILib\\app\\.cxx\\Debug\\n6l545p2\\arm64-v8a" ^
-  xrimguilib
